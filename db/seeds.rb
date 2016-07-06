0210cc83139ac3764f0e1e01d8c3f9fe367b2f08
ma-Rac <mno.racca@gmail.com>
2016-07-06 12:56:18 -0300

    pushing to heroku again

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Todo.create(title: "get a json correctly", completed: false)
