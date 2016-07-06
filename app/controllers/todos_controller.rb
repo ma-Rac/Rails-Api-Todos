b756d1ec53adae15918e6f2e3db33beebdaf9101
ma-Rac <mno.racca@gmail.com>
2016-07-06 14:35:24 -0300

    Sigh... more fixes from people that know than me, and a me that knows more than a previous me

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,63 +1,57 @@
 class TodosController < ApplicationController
   before_action :set_todo, only: [:show, :update, :destroy]
 
-  # GET /todos
-  # GET /todos.json
+
+  # GET /todos.json... no other options, just JSON.
   def index
     @todo = Todo.new
     @todos = Todo.all
+    render json: @todos
   end
 
   def show
   end
 
-  # POST /todos
-  # POST /todos.json
+
+  # POST /todos.json and only json because Json is boss in this API
   def create
     @todo = Todo.new(todo_params)
 
-    respond_to do |format|
+
       if @todo.save
-        format.html { redirect_to root_path, notice: 'Todo was successfully created.' }
-        format.json { render :show, status: :created, location: @todo }
+        format.json { render json: @todo, status: :created, location: @todo }
       else
-        format.html { render :index }
         format.json { render json: @todo.errors, status: :unprocessable_entity }
       end
-    end
+
   end
 
-  # PATCH/PUT /todos/1
-  # PATCH/PUT /todos/1.json
+#I dont need patches.. but I'll leave this here JUST IN CASE.
   def update
     respond_to do |format|
       if @todo.update(todo_params)
-        format.html { redirect_to @todo, notice: 'Todo was successfully updated.' }
-        format.json { render :show, status: :ok, location: @todo }
+        format.json { render json: @todo, status: :ok, location: @todo }
       else
-        format.html { render :index }
         format.json { render json: @todo.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /todos/1
-  # DELETE /todos/1.json
+#Dont... don't do this.. It's only a todo.. you monster
   def destroy
     @todo.destroy
     respond_to do |format|
-      format.html { redirect_to root_url, notice: 'Todo was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    # Use callbacks to share common setup or constraints between actions. #plus.. it's private.
     def set_todo
       @todo = Todo.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    # Never trust parameters from the scary internet, only allow the white list through. #this comment is amazing.
     def todo_params
       params.require(:todo).permit(:title, :completed)
     end
